0410caa4fcf918920e5005c016a3fad64c8af044
vajdzius <vajdzius@gmail.com>
2017-11-17 18:37:59 +0200

    zzzz

--- Flights.php
+++ Flights.php
@@ -4,23 +4,14 @@ calculatePrices (250000, 0.3);
 
 function calculatePrices ($costs, $margin)
 {
-	var FIRST_CLASS = 10;
-	var BUSINESS_CLASS = 30;
-	var ECONOMY_CLASS = 280;
 	define ("FIRST_CLASS", "10");
 	define ("BUSINESS_CLASS", "30");
 	define ("ECONOMY_CLASS", "280");
 
-	var PRICE_FIRST = 10000;
-	var PRICE_BUSINESS = 4000;
-	var PRICE_ECONOMY = 500;
 	define ("PRICE_FIRST", "10000");
 	define ("PRICE_BUSINESS", "4000");
 	define ("PRICE_ECONOMY", "678");
 
-	var FILL_FIRST = 1;
-	var FILL_BUSINESS = 0.8;
-	var FILL_ECONOMY = 0.68;
 	define ("FILL_FIRST", "1");
 	define ("FILL_BUSINESS", "0.8");
 	define ("FILL_ECONOMY", "0.68");
@@ -37,5 +28,3 @@ function calculatePrices ($costs, $margin)
 	echo "Price of BUSINESS_CLASS&nbsp;" . (PRICE_BUSINESS) . "<br>";
 	echo "Price of ECONOMY_CLASS&nbsp;" . (PRICE_ECONOMY) . "<br>";
 }
-
-?>
